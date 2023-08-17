dc75f32e230e56f1128819c4cdd56c97841818cf
Pankaj <pankajbeniwal3112@gmail.com>
2023-08-17 13:52:59 +0530

    connect  to mongoatlas

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,14 @@
-//require library
 const mongoose = require('mongoose');
 
-//connect to database
-async function main() {
-    const db = await mongoose.connect('mongodb+srv://pankaj3112:ftHLwuuPqJvyE9Qj@cluster0.lm6iyo2.mongodb.net/?retryWrites=true&w=majority');
-    module.exports = db;
+async function main(){
+    try{
+        const db = await mongoose.connect('mongodb+srv://pankaj3112:ftHLwuuPqJvyE9Qj@cluster0.lm6iyo2.mongodb.net/?retryWrites=true&w=majority');
+        module.exports = db;
+        console.log('**** MongoDB Connected ****')
+    }
+    catch(err){
+        console.log("****Error in connecting to db ----> ", err);
+    }
 }
 
-main()
-.then(() => console.log('Connected to MongoDB...')) //if connected
-.catch(err => console.log(err)); //if error
+main();
